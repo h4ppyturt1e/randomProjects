55f5f60b2b714c31ce8cfc1ff67502308496275d
h4ppyturt1e <ryan170402@gmail.com>
2021-07-11 10:26:16 +0700

    added morse_translator
    and added exception catching to some existing functions

--- keep_cheating_and_nobody_explodes.py
+++ keep_cheating_and_nobody_explodes.py
@@ -1,23 +1,17 @@
 def passwords():
     words = ['about', 'after', 'again', 'below', 'could', 'every', 'first', 'found', 'great', 'house', 'large', 'learn',
              'never', 'other', 'place', 'plant', 'point', 'right', 'small', 'sound', 'spell', 'still', 'study', 'their',
-             'there', 'these', 'thing', 'think', 'three', 'water', 'where', 'which', 'world', 'would', 'write', 'stop']
+             'there', 'these', 'thing', 'think', 'three', 'water', 'where', 'which', 'world', 'would', 'write']
 
     first_letters = input("First letters: ").split()
-    third_letters = input("Third letters: ").split()
-    fifth_letters = input("Fifth letters: ").split()
+    possible_words_first, possible_words_fourth = [], []
 
-    possible_words_first, possible_words_third, possible_words_fifth = [], [], []
     [[possible_words_first.append(word) for word in words if word[0] == letter] for letter in first_letters]
     print(possible_words_first)
-    [[possible_words_third.append(word) for word in possible_words_first if word[2] == letter] for letter in
-     third_letters]
-    if len(possible_words_third) >= 2:
-        [[possible_words_fifth.append(word) for word in possible_words_third if word[4] == letter] for letter in
-         fifth_letters]
-        print("Answer:\n{}".format(possible_words_fifth))
-    else:
-        print("Answer:\n{}".format(possible_words_third))
+    fourth_letters = input("Third letters: ").split()
+    [[possible_words_fourth.append(word) for word in possible_words_first if word[2] == letter] for letter in
+     fourth_letters]
+    print("Answer:\n{}".format(possible_words_fourth))
 
 
 def complicated_wires(is_odd, has_parallel, num_batteries):
@@ -84,13 +78,40 @@ def words_game():
                   "YOUR": "UH UH, YOU ARE, UH HUH, YOUR, NEXT, UR, SURE, U, YOU'RE, YOU, WHAT?, HOLD, LIKE, DONE"}
 
     while True:
-        position = position_dict[int(label_dict[input("Enter given word ").lower()])]
-        print(position)
-        order = order_dict[input("Enter word: ").upper()]
-        print(order)
+        try:
+            position = position_dict[int(label_dict[input("Enter given word ").lower()])]
+            print(position)
+            order = order_dict[input("Enter word: ").upper()]
+            print(order)
+        except KeyError:
+            print("Typo, try again.")
+            words_game()
+
+
+def morse_translator():
+    morse_dict = {'.-': 'A', '-...': 'B', '-.-.': 'C', '-..': 'D', '.': 'E',
+                  '..-.': 'F', '--.': 'G', '....': 'H', '..': 'I', '.---': 'J',
+                  '-.-': 'K', '.-..': 'L', '--': 'M', '-.': 'N', '---': 'O',
+                  '.--.': 'P', '--.-': 'Q', '.-.': 'R', '...': 'S', '-': 'T',
+                  '..-': 'U', '...-': 'V', '.--': 'W', '-..-': 'X', '-.--': 'Y',
+                  '--..': 'Z', '-----': '0', '.----': '1', '..---': '2', '...--':
+                  '3', '....-': '4', '.....': '5', '-....': '6', '--...': '7',
+                  '---..': '8', '----.': '9'}
+    result = ""
+
+    prompt = input("Enter morse code separated by whitespace: ").split()
+    for item in prompt:
+        try:
+            result += morse_dict[item]
+        except KeyError:
+            print("Unknown morse. Try again.")
+
+    print(result)
 
 
 def main():
+    # TODD: GET PARSING BOMB DATA WORKING, MAKE GAME CHOOSER SIMPLER
+    """
     vowels = ['a', 'e', 'i', 'o', 'u']
 
     serial_num = input("Serial number? ").split()
@@ -99,10 +120,8 @@ def main():
     num_batteries = int(input("Number of batteries? "))
     has_parallel = True if input("Has parallel port? ").lower() in ["yes", "y"] else False
     print()
-
-    complicated_wires(is_odd, has_parallel, num_batteries)
-    # while True:
-    #     passwords()
+    """
+    morse_translator()
 
 
 if __name__ == "__main__":
--- test.py
+++ test.py
@@ -1,38 +1,19 @@
+words = ['about', 'after', 'again', 'below', 'could', 'every', 'first', 'found', 'great', 'house', 'large', 'learn',
+         'never', 'other', 'place', 'plant', 'point', 'right', 'small', 'sound', 'spell', 'still', 'study', 'their',
+         'there', 'these', 'thing', 'think', 'three', 'water', 'where', 'which', 'world', 'would', 'write']
 
-def passwords():
-    words = ['about', 'after', 'again', 'below', 'could', 'every', 'first', 'found', 'great', 'house', 'large', 'learn',
-             'never', 'other', 'place', 'plant', 'point', 'right', 'small', 'sound', 'spell', 'still', 'study', 'their',
-             'there', 'these', 'thing', 'think', 'three', 'water', 'where', 'which', 'world', 'would', 'write', 'stop']
+tuple_list = []
+for i in range(5):
+    for j in range(5):
+        l = [word[i] + word[j] for word in words]
+        s = set(l)
+        diff = len(l) - len(s)
+        tuple_list.append((diff, i+1, j+1))
 
-    first_letters = input("First letters: ").split()
-    third_letters = input("Third letters: ").split()
-    fifth_letters = input("Fifth letters: ").split()
+tuple_list.sort()
 
-    possible_words_first, possible_words_third, possible_words_fifth = [], [], []
-    [[possible_words_first.append(word) for word in words if word[0] == letter] for letter in first_letters]
-    print(possible_words_first)
-    [[possible_words_third.append(word) for word in possible_words_first if word[2] == letter] for letter in third_letters]
-    if len(possible_words_third) >= 2:
-        [[possible_words_fifth.append(word) for word in possible_words_third if word[4] == letter] for letter in fifth_letters]
-        print("Answer:\n{}".format(possible_words_fifth))
-    else:
-        print("Answer:\n{}".format(possible_words_third))
+[print(item) for item in tuple_list]
+print("\nBest combination:\n====================")
+print(tuple_list[0])
 
 
-def complicated_wires():
-    # led,red,blue,star -> instruction
-    # 1001,b
-    with open("complicated_wires_cheatsheet.csv") as f:
-        data = f.readlines()
-    print(data)
-    prompt = input()
-
-
-def main():
-    complicated_wires()
-    # while True:
-    #     passwords()
-
-
-if __name__ == "__main__":
-    main()
