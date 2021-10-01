42e844007f304a61411ba4a0dc9f767188897503
h4ppyturt1e <ryan170402@gmail.com>
2021-09-30 20:14:25 -0700

    refactored get_final_binary and bad variable names

--- signed_number_calculator.py
+++ signed_number_calculator.py
@@ -7,38 +7,51 @@ def binary_addition():
     num2 = input("Enter second binary number: ")
     bits = len(num1)
     largest_bit = 2 ** (bits-1)
+
     try:
-        if num1[0] == "1":
-            num1 = to_dec(num1[1:], 2) - largest_bit
-        else:
-            num1 = to_dec(num1[1:], 2)
-        if num2[0] == "1":
-            num2 = to_dec(num2[1:], 2) - largest_bit
-        else:
-            num2 = to_dec(num2[1:], 2)
+        nums_bool = [num[0] == '1' for num in [num1, num2]]
+        num1 = to_dec(num1[1:], 2) - largest_bit if nums_bool[0] else to_dec(num1[1:], 2)
+        num2 = to_dec(num2[1:], 2) - largest_bit if nums_bool[1] else to_dec(num2[1:], 2)
+
     except IndexError:
         print("Invalid input, try again.\n")
         binary_addition()
+    
     total = num1 + num2
-    print(num1, num2, total)
+    print(f"{num1} + {num2} = {total}")
+
     if int(total) >= largest_bit:
         print(total, "is greater than", largest_bit, ", Incorrect sum.")
+        possible_total = total - largest_bit
+        possible_answer = get_final_binary(possible_total, bits)
+        print(f"Possible answer: {possible_answer}")
+
     else:
-        new_total = convert_base(str(abs(total)), 10, 2)
-        while len(new_total) != bits:
-            new_total = "0" + new_total
-        print(new_total)
-        if new_total[0] == "1":
-            new_new_total = []
-            for i in range(bits):
-                if new_total[i] == "1":
-                    new_new_total.append("0")
-                else:
-                    new_new_total.append("1")
-            total_cache = convert_base(str(to_dec(''.join(new_new_total), 2) + 1), 10, 2)
-        else:
-            total_cache = new_total
-        print("Answer:", total_cache)
+        answer = get_final_binary(total, bits)
+        print("Answer:", answer)
+
+
+def get_final_binary(total, bits):
+    converted_total = convert_base(str(abs(total)), 10, 2)
+    
+    while len(converted_total) != bits:
+        converted_total = "0" + converted_total
+        
+    if converted_total[0] == "1":
+        normalized_total = []
+        for i in range(bits):
+            if converted_total[i] == "1":
+                normalized_total.append("0")
+            else:
+                normalized_total.append("1")
+
+        final_total = convert_base(str(to_dec(''.join(normalized_total), 2) + 1), 10, 2)
+
+    else:
+        final_total = converted_total
+
+    return final_total
+
 
 
 if __name__ == "__main__":
